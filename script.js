7649bfddb09d6f1be732e0245aed40a0883a7462
MD.SAQLAIN <mdsaqlain2486@gmail.com>
2025-10-15 19:49:20 +0530

    for four

--- script.js
+++ script.js
@@ -13,7 +13,15 @@
 
 //Example 3
 
-document.getElementById("changeOrder").addEventListener("click", function () {
-  let coffeeType = document.getElementById("coffeeType");
-  coffeeType.textContent = "Espresso";
+// document.getElementById("changeOrder").addEventListener("click", function () {
+//   let coffeeType = document.getElementById("coffeeType");
+//   coffeeType.textContent = "Espresso";
+// });
+
+//Example 4
+
+document.getElementById("newItem").addEventListener("click", function () {
+  let newItem = document.createElement("li");
+  newItem.textContent = "Eggs";
+  document.getElementById("shoppingList").appendChild(newItem);
 });
