21fb4244feee28caec345ff72558ab23d423bbdd
MD.SAQLAIN <mdsaqlain2486@gmail.com>
2025-10-15 19:40:04 +0530

    for three

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// EXAMPLE 1
-// document.getElementById("clickthis").addEventListener("click", function () {
+// // EXAMPLE 1
+// document.getElementById("clickThis").addEventListener("click", function () {
 //   let paragraph = document.getElementById("Mypara");
 //   paragraph.textContent = "the paragraph is changed!!";
 // });
 
-// EXAMPLE 2
+// // EXAMPLE 2
 
-document.getElementById("clickMe").addEventListener("click", function () {
-  let citiesList = document.getElementById("citiesList");
-  citiesList.firstElementChild.classList.add("highlight");
+// document.getElementById("clickMe").addEventListener("click", function () {
+//   let citiesList = document.getElementById("citiesList");
+//   citiesList.firstElementChild.classList.add("highlight");
+// });
+
+//Example 3
+
+document.getElementById("changeOrder").addEventListener("click", function () {
+  let coffeeType = document.getElementById("coffeeType");
+  coffeeType.textContent = "Espresso";
 });
